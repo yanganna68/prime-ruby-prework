df528971093c4a5802ccff059df1501c2b9b56af
ANNA YANG <yanganna68@gmail.com>
2017-12-20 21:06:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,11 @@
-def prime?(int)
-  if int.abs < 3
-    return false
-  else
-    (2..int.abs).to_a.each do |num|
-      if int % num == 0
-        return false
-      else
-        return true
-      end
-  end
-end
+def prime?(num)
+   if num.abs < 3
+     return false
+   end
+   (2...num.abs).to_a.each do |current|
+    if num % current == 0
+       return false
+     end
+   end
+   true
 end
