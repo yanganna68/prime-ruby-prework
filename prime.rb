bb2c68c3f0c0c5105f400bf76b9dddd8695eed60
ANNA YANG <yanganna68@gmail.com>
2017-12-20 21:06:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+   if num.abs < 3
+     return false
+   end
+   (2...num.abs).to_a.each do |current|
+    if num % current == 0
+       return false
+     end
+   end
+   true
+end
